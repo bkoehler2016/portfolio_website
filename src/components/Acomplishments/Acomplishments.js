dbafc41fd060356b5a3b71741661597a62b6831e
Ben Koehler <bkoehler85282@gmail.com>
2021-06-14 14:02:16 -0700

    first commit

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
+
+const data = [
+  { number: 100, text: "Open Source Projects" },
+  { number: 200, text: "Hours of Web Development" },
+  { number: 25, text: "Github Followers" },
+  { number: 50, text: "Github Stars" },
+];
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Acomplishments;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem,
+        veritatis.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
