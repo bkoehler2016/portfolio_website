546b6243c7141d06a2eca962c4637db00b6df253
Benjamin Koehler <bkoehler751@gmail.com>
2022-03-12 13:08:53 -0700

    fixed mispelled words

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,49 @@
 export const projects = [
   {
-    title: 'Linkedin Clone',
-    description: "Linkedin clone following the same formats as Linkedin built with firebase for the backend",
-      image: '/images/Linkedin_clone.png',
-      tags: ['React', 'Firebase' ,'Redux', 'Javascript'],
-    source: 'https://github.com/bkoehler2016/linkedin-clone',
-    visit: 'https://linkedin-clone-bk.netlify.app/',
+    title: "Linkedin Clone",
+    description:
+      "Linkedin clone following the same formats as Linkedin built with firebase for the backend",
+    image: "/images/Linkedin_clone.png",
+    tags: ["React", "Firebase", "Redux", "Javascript"],
+    source: "https://github.com/bkoehler2016/linkedin-clone",
+    visit: "https://linkedin-clone-bk.netlify.app/",
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description: "A demo store that allows you purchase items and checkout with credit card infromation. Sends confirmation emails to both parties",
-    image: '/images/store.png', 
-    tags: ['React', 'JavaScript'],
-    source: 'https://github.com/bkoehler2016/shopping-cart',
-    visit: 'https://benstore.netlify.app/', 
+    title: "E-Commerce",
+    description:
+      "A demo store that allows you purchase items and checkout with credit card infromation. Sends confirmation emails to both parties",
+    image: "/images/store.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/bkoehler2016/shopping-cart",
+    visit: "https://benstore.netlify.app/",
     id: 1,
   },
   {
-    title: 'KidsFlyy',
+    title: "KidsFlyy",
     description: "A basic web page brochure showcaseing a front-end site.",
-      image: '/images/KidsFlyy_Landing.JPG',
-      tags: ['HTML', 'CSS'],
-    source: 'https://github.com/KidsFlyy/Marketing',
-    visit: 'https://peaceful-bell-23f2fd.netlify.app/',
+    image: "/images/KidsFlyy_Landing.JPG",
+    tags: ["HTML", "CSS"],
+    source: "https://github.com/KidsFlyy/Marketing",
+    visit: "https://peaceful-bell-23f2fd.netlify.app/",
     id: 2,
   },
   {
-    title: 'Blog Post',
+    title: "Blog Post",
     description: "A simple blog site built with GatsbyJS",
-      image: '/images/blog_site.png',
-      tags: ['HTML', 'CSS', 'Javascipt', 'React', 'Gatsby'],
-    source: 'https://github.com/bkoehler2016/my-new-gatsby-site',
-    visit: 'https://mynewgatsbysitemain90394.gatsbyjs.io/',
+    image: "/images/blog_site.png",
+    tags: ["HTML", "CSS", "Javascipt", "React", "Gatsby"],
+    source: "https://github.com/bkoehler2016/my-new-gatsby-site",
+    visit: "https://mynewgatsbysitemain90394.gatsbyjs.io/",
     id: 3,
   },
- 
 ];
 
 export const TimeLineData = [
-  { year: 2019, text: 'Started my journey with Lambda School', },
-  { year: 2020, text: 'Ironed out my developer skills', },
-  { year: 2021, text: 'Grdurated from Lambda and started applying to developer roles', },
-  
+  { year: 2019, text: "Started my journey with Lambda School" },
+  { year: 2020, text: "Ironed out my developer skills" },
+  {
+    year: 2021,
+    text: "Graduated from Lambda and started applying to developer roles",
+  },
 ];
